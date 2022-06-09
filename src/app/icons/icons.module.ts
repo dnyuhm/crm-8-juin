585b5529eb4876f933dc8ff0226b5da9e73cd139
dnyuhm <dylanmeuriot@gmail.com>
2022-06-09 14:12:26 +0200

    Commit 9 : icons components et FontAwesome

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconNavComponent } from './icon-nav/icon-nav.component';
+import { IconCloseComponent } from './icon-close/icon-close.component';
+import { IconEditComponent } from './icon-edit/icon-edit.component';
+import { IconDeleteComponent } from './icon-delete/icon-delete.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    IconNavComponent,
+    IconCloseComponent,
+    IconEditComponent,
+    IconDeleteComponent
+  ],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [
+    IconNavComponent,
+    IconCloseComponent,
+    IconEditComponent,
+    IconDeleteComponent
+  ],
 })
-export class IconsModule { }
+export class IconsModule {}
