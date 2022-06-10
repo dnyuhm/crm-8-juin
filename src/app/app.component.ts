e0911bcd75a229e60075b9620824a9d2f387f852
dnyuhm <dylanmeuriot@gmail.com>
2022-06-10 14:53:49 +0200

    Commit 21 : Observable, Subject & BehaviorSubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string;
+  private obs: Observable<any>;
+  private subj: Subject<any>;
+  private behav: BehaviorSubject<any>;
 
   constructor() {
     this.title = 'crm';
+    this.obs = new Observable((listX) => {
+      listX.next(Math.random());
+    });
+    this.subj = new Subject();
+    this.behav = new BehaviorSubject('toto');
+
+    // this.obs.subscribe((data) => console.log(data));
+    // this.obs.subscribe((data) => console.log(data));
+
+    // this.subj.subscribe((data) => console.log(data));
+    // this.subj.next('tutu');
+    // this.subj.subscribe((data) => console.log(data));
+    // this.subj.next(Math.random());
+
+    this.behav.next('toto2');
+    this.behav.subscribe((data) => console.log(data));
+    this.behav.next('toto3');
+    this.behav.next(Math.random());
+    this.behav.subscribe((data) => console.log(data));
   }
 }
